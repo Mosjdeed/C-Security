4194ea41d98740b3b1ce40cd609486def846c093
Mosjdeed <wmostafa452@gmail.com>
2025-02-27 18:56:48 +0200

    Update HTML configuration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePageOne from "./pages/HomePageOne";
 import HomePageTwo from "./pages/HomePageTwo";
 import './styles/main.css';
@@ -14,16 +14,22 @@ import ViewProfilePage from "./pages/ViewProfilePage";
 import RouteScrollToTop from "./helper/RouteScrollToTop";
 import ChatPage from "./pages/ChatPage";
 
+
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/C-Security">
       <RouteScrollToTop />
       <Routes>
         <Route exact path='/' element={<HomePageOne />} />
         <Route exact path='/index-2' element={<HomePageTwo />} />
+
+        {/* SL */}
         <Route exact path='/add-user' element={<AddUserPage />} />
         <Route exact path='/assign-role' element={<AssignRolePage />} />
+        {/* <Route exact path='/email' element={<EmailPage />} /> */}
         <Route exact path='/forgot-password' element={<ForgotPasswordPage />} />
+
+
         <Route exact path='/portfolio' element={<PortfolioPage />} />
         <Route exact path='/role-access' element={<RoleAccessPage />} />
         <Route exact path='/sign-in' element={<SignInPage />} />
@@ -31,9 +37,10 @@ function App() {
         <Route exact path='/users-list' element={<UsersListPage />} />
         <Route exact path='/view-profile' element={<ViewProfilePage />} />
         <Route exact path='/chat' element={<ChatPage />} />
+
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
-export default App;   
+export default App;
